da2074317c796f9dcab259d7376850ecef080959
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2021-02-11 14:31:56 -0300

    feat: :ok_hand: init project with CRA and added eslint and prettier

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container, Title, Build } from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <Title>Turn the lights ON/OFF</Title>
+
+      <Build />
+    </Container>
+  );
+};
+
+export default Home;
---
+++ src/styles/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  body {
+    font-family: 'Ubuntu', sans-serif;
+  }
+`;
